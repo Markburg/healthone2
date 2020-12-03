8a83d7c7de2073cea2f95c84b62ffea8c12437d4
Your Name <you@example.com>
2020-12-03 15:00:09 +0100

    twig completed

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,9 +5,10 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,7 +21,11 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public function show($slug) {
-        return new Response(sprintf('Future space to show one space article: %s', $slug));
+        $comments = ['opmerking1', 'opmerking2', 'opmerking3'];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
 
     }
 
