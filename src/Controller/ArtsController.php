a5167ca3d494da795efd73a34c0edda43d2847c5
Your Name <you@example.com>
2021-01-21 13:56:42 +0100

    arts en medewerker af en begonnen met admin

--- src/Controller/ArtsController.php
+++ src/Controller/ArtsController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Medicijn;
+use App\Entity\Patient;
+use App\Entity\Recept;
+use App\Form\MedicijnType;
+use App\Form\PatientType;
+use App\Form\ReceptType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +24,72 @@ class ArtsController extends AbstractController
         return $this->render('arts/index.html.twig', [
             'controller_name' => 'ArtsController',
         ]);
+    }
+    /**
+     * @Route ("/arts/recepten"), name="arts_recepten")
+     * @return Response
+     */
+    public function receptenAction(): Response
+    {
+        $recepten = $this->getDoctrine()->getRepository(Recept::class)->findAll();
+        return $this->render('arts/recepten.html.twig', [
+            'recepten' => $recepten,
+        ]);
+    }
+    /**
+     * @Route ("/arts/recept/new"), name="arts_recept_new")
+     */
+    public function newRecept(Request $request): Response
+    {
+        $form = $this->createForm(ReceptType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $recept = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($recept);
+            $em->flush();
+            $this->addFlash('succes','Recept gemaakt!');
+
+            return $this->redirectToRoute('app_arts_recepten');
+        }
+        return $this->render('arts/new_recept.html.twig', ['receptForm' => $form->createView(),
+        ]);
+    }
+    /** *@Route("/arts/recept/{id}/edit", name="arts_recept_edit") */
+    public function editRecept(Recept $recept,Request $request, EntityManagerInterface $em) {
+        $form = $this->createForm(ReceptType::class, $recept);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($recept);
+            $em->flush();
+            $this->addFlash('succes','Recept Update!');
+
+            return $this->redirectToRoute('app_arts_recepten',[
+                'id'=>$recept->getId(),
+            ]);
+        }
+
+        return $this->render('arts/edit_recept.html.twig', ['receptForm'=>$form->createView()]);
+    }
+    /** *@Route("/arts/recept/delete", name="arts_recept_delete") */
+    public function deleteRecept($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        if (!$id)
+        {
+            throw $this->createNotFoundException('No ID found');
+        }
+        $recept = $em->getRepository(Recept::class)->Find($id);
+
+        if($recept != null)
+        {
+            $em->remove($recept);
+            $em->flush();
+            $this->addFlash('succes','recept is verwijderd!');
+        }
+
+        return $this->redirectToRoute('app_arts_recepten');
     }
 }
--- src/Controller/MedewerkerController.php
+++ src/Controller/MedewerkerController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Medicijn;
+use App\Form\MedicijnType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +20,64 @@ class MedewerkerController extends AbstractController
         return $this->render('medewerker/index.html.twig', [
             'controller_name' => 'MedewerkerController',
         ]);
+    }
+    /** * @Route("/medewerker/medicijnen", name="medewerker_medicijnen") */
+    public function getMedicijnen()
+    {
+        $medicijnen = $this->getDoctrine()->getRepository(Medicijn::class)->getMedicijnen();
+        return $this->render('medewerker/medicijnen.html.twig', ['medicijnen' => $medicijnen]);
+    }
+    /** *@Route("/medewerker/medicijn/new", name="medewerker_medicijn_new") */
+    public function newMedicijn(Request $request){
+        $form = $this->createForm(MedicijnType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $medicijn = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($medicijn);
+            $em->flush();
+            $this->addFlash('succes','Medicijn gemaakt!');
+
+            return $this->redirectToRoute('medewerker_medicijnen');
+        }
+
+        return $this->render('medewerker/new_medicijn.html.twig', ['medicijnForm'=>$form->createView()]);
+    }
+    /** *@Route("/medewerker/medicijn/{id}/edit", name="medewerker_medicijn_edit") */
+    public function editMedicijn(Medicijn $medicijn,Request $request, EntityManagerInterface $em) {
+        $form = $this->createForm(MedicijnType::class, $medicijn);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($medicijn);
+            $em->flush();
+            $this->addFlash('succes','Medicijn Update!');
+
+            return $this->redirectToRoute('medewerker_medicijnen',[
+                'id'=>$medicijn->getId(),
+            ]);
+        }
+
+        return $this->render('medewerker/edit_medicijn.html.twig', ['medicijnForm'=>$form->createView()]);
+    }
+    /** * @Route("/medewerker/medicijnen/delete/{id}", name="medewerker_delete_medicijn") */
+    public function deleteMedicijn($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        if (!$id)
+        {
+            throw $this->createNotFoundException('No ID found');
+        }
+        $medicijn = $em->getRepository(Medicijn::class)->Find($id);
+
+        if($medicijn != null)
+        {
+            $em->remove($medicijn);
+            $em->flush();
+            $this->addFlash('succes','Medicijn is verwijderd!');
+        }
+
+        return $this->redirectToRoute('medewerker_medicijnen');
     }
 }
